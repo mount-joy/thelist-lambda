c54c3f0e54f39cee2e35afb21630146567953980
Alex Naylor <Alexander.Naylor@ibm.com>
2020-11-19 19:15:15 +0000

    Review comments

---
+++ cors/headers.go
@@ -0,0 +1,7 @@
+package cors
+
+const originHeader = "origin"
+const allowMethodsHeader = "Access-Control-Allow-Methods"
+const allowOriginHeader = "Access-Control-Allow-Origin"
+const maxAgeHeader = "Access-Control-Max-Age"
+const allowHeadersHeader = "Access-Control-Allow-Headers"
